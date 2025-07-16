e9abf2655ea14c220fe05b1ba6b6df59f5c3a9c1
eunseo04 <jes040616@naver.com>
2025-07-16 16:53:22 +0900

    Change

--- src/main/java/org/example/EssentialProject/Lv3/Kiosk.java
+++ src/main/java/org/example/EssentialProject/Lv3/Kiosk.java
@@ -1,17 +1,16 @@
 package org.example.EssentialProject.Lv3;
 
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.List;
 
 public class Kiosk {
     private final List<MenuItem> menuItems = new ArrayList<>();
 
-    Kiosk() {
-        menuItems.add(new MenuItem("ShackBurger", 6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
+    Kiosk(MenuItem a, MenuItem b, MenuItem c, MenuItem d) {
+        this.menuItems.add(a);
+        this.menuItems.add(b);
+        this.menuItems.add(c);
+        this.menuItems.add(d);
     }
 
     public void menu() {
@@ -27,15 +26,31 @@ public class Kiosk {
         switch(num) {
             case 1:
                 System.out.println("ShackBurger을(를) 선택하셨습니다.");
+                System.out.print(menuItems.get(0).name + " | ");
+                System.out.print(menuItems.get(0).price + " | ");
+                System.out.print(menuItems.get(0).explain);
+                System.out.println();
                 break;
             case 2:
                 System.out.println("SmokeShack을(를) 선택하셨습니다.");
+                System.out.print(menuItems.get(1).name + " | ");
+                System.out.print(menuItems.get(1).price + " | ");
+                System.out.print(menuItems.get(1).explain);
+                System.out.println();
                 break;
             case 3:
                 System.out.println("Cheeseburger을(를) 선택하셨습니다.");
+                System.out.print(menuItems.get(2).name + " | ");
+                System.out.print(menuItems.get(2).price + " | ");
+                System.out.print(menuItems.get(2).explain);
+                System.out.println();
                 break;
             case 4:
                 System.out.println("Hamburger을(를) 선택하셨습니다.");
+                System.out.print(menuItems.get(3).name + " | ");
+                System.out.print(menuItems.get(3).price + " | ");
+                System.out.print(menuItems.get(3).explain);
+                System.out.println();
                 break;
             default:
                 System.out.println("있는 메뉴를 선택해주세요!");
