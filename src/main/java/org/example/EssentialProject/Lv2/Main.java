9b8b0cda37d85395abc9df8d9fff6886a2cda733
eunseo04 <127182224+eunseo04@users.noreply.github.com>
2025-07-24 13:57:48 +0900

    Update Main.java

--- src/main/java/org/example/EssentialProject/Lv2/Main.java
+++ src/main/java/org/example/EssentialProject/Lv2/Main.java
@@ -9,15 +9,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<MenuItem> menuItems = new ArrayList<>();
-        MenuItem a = new MenuItem("ShackBurger", 6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-        MenuItem b = new MenuItem("SmokeShack", 8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-        MenuItem c = new MenuItem("Cheeseburger", 6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-        MenuItem d = new MenuItem("Hamburger", 5.4,"비프패티를 기반으로 야채가 들어간 기본버거");
-
-        menuItems.add(a);
-        menuItems.add(b);
-        menuItems.add(c);
-        menuItems.add(d);
+        menuItems.add(new MenuItem("ShackBurger", 6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
 
         for  (MenuItem menuItem : menuItems) {
             System.out.print(menuItem.name + " | ");
